d299132bc07c82a169668263c3a0bcae6da1d08a
mahesh-digitalflake <124568335+mahesh-digitalflake@users.noreply.github.com>
2023-06-30 15:13:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('heloo.....hi ------->---->Hello Mahesh  You are  in AWS  Cloud. THis is TECHESSAY, Please share and Subscribe and like my Videos'); //write a response to the client
+  res.write('heloo.....hi ------->---->Hello Mahesh-----  You are  in AWS  Cloud. THis is TECHESSAY, Please share and Subscribe and like my Videos'); //write a response to the client
   res.end(); //end the response
 }).listen(5000); //the server object listens on port 4000
