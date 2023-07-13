bf68e2869b57f7de2d38303fd4dbc772df70a0b6
mahesh-digitalflake <124568335+mahesh-digitalflake@users.noreply.github.com>
2023-07-13 12:45:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello Good Morning...'); //write a response to the client
+  res.write('Hello Good Afternoon...'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
